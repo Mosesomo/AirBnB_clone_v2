db248eb8851767ca0f90a23fcfe8ef05ff75e5ba
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-11-21 01:50:43 +0300

    adding id

--- models/user.py
+++ models/user.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+from models.base_model import Base
 
-from sqlalchemy import Column, String
-from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-    User class that inherits from BaseModel and Base
-    """
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
+    id = Column(String(60), primary_key=True)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
