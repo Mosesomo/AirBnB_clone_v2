3481226c8cec85f177dc096179f0674a6ba4f77f
Mosesomo <moseswamboga4@gmail.com>
2023-11-22 02:26:56 -0800

    changes

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
 from models.base_model import Base
+from os import getenv
 
 
 class User(BaseModel, Base):
@@ -17,12 +18,19 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = "users"
-    id = Column(String(60), primary_key=True)
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        id = Column(String(60), primary_key=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", cascade='all, delete, delete-orphan',
+                              backref="user")
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="user")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
