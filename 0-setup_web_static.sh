455e8566579befb898c7b62d56132ae75b2f2648
Mosesomo <moseswamboga4@gmail.com>
2023-12-06 23:37:35 -0800

    changes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,44 +1,42 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+# script that sets up your web servers for the deployment of web_static
 
-# Installing nginx
-apt-get update
-apt-get install -y nginx
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Creating folders
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo  mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+      Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# testing our nginx congiguration with a simple content
-echo "Setting up our web servers" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# creating a symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Ownerships and groups
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-# updating nginx
-printf %s "server {
+sudo chown -R ubuntu:ubuntu /data/
+printf "%s\n" "server {
      listen 80 default_server;
      listen [::]:80 default_server;
-     add_header X-Served-By $HOSTNAME;
+     add_header X-Served-By \$HOSTNAME;
      root /var/www/html;
      index index.html index.htm;
      location /hbnb_static {
-     	alias /data/web_static/current;
-	index index.html index.htm
+	alias /data/web_static/current;
+	index index.html index.htm;
      }
      location /redirect_me {
-     	return 301 https://github.com/Mosesomo;
+	return 301 https://github.com/Mosesomo;
      }
-     error_page 404 /404.html
+     error_page 404 /404.html;
      location /404 {
-     	root /var/www/html;
+	root /var/www/html;
 	internal;
      }
-}" > /etc/nginx/sites-available/default
+}" | sudo tee /etc/nginx/sites-available/default
 
-# restarting our server
-service nginx restart
+sudo service nginx restart
