81d59a4fa4c2cbb387b8ada4defbd327e6ffea21
Mosesomo <moseswamboga4@gmail.com>
2023-12-26 00:14:54 +0300

    id

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application
+"""starts a Flask web application:
 """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
     """display a HTML page
     """
 
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda s: s.name)
-    return render_template('8-cities_by_states.html', states=sorted_states)
+    if id is not None:
+        id = "{}.{}".format(State, id)
+    return render_template('9-states.html', states=states, state_id=id)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """method to handle @app.teardown_appcontext
+    """Declare a method to handle @app.teardown_appcontext
     """
 
     storage.close()
@@ -27,4 +30,3 @@ def teardown_db(exception):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
