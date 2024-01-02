77475be0a3e4f276cbcbbfa9cfb8989da9b62298
Mosesomo <moseswamboga4@gmail.com>
2024-01-02 03:50:07 -0800

    changing dump sql

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application
-"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -26,5 +24,5 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
